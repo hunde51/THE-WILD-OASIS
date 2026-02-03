b7585f221513151fe169cb509d60ce838bb6fdf9
blen <hundetesfa6@gmail.com>
2026-02-03 10:45:43 +0300

    main refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
